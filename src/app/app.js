f4efb3b5d8df87721a6dd04c41d2a1708f25415e
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-09 08:55:44 -0500

    append div

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-        <div><h1>Hello, world!</h1><p>eat my butt</p></div>,
+const NEW_DIV = (
+  <div className="block">
+    <button>X</button>
+  </div>
+);
+let boxBtn = document.getElementById('makeBox');
+
+boxBtn.addEventListener("click", function() {
+    makeABox();
+  }, false);
+
+
+
+function makeABox() {
+  ReactDOM.render(
+        NEW_DIV,
         document.getElementById('app')
       );
+}
